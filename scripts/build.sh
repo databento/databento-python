769f2cab038b3c6dce4384211fe86758b661dc7c
Carter Green <carter@databento.com>
2022-12-09 08:53:51 -0600

    FIX: Fix databento-python `build.sh`

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 set -e
-python -m pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
-pip install -r requirements_dev.txt
-pip install .
+pip3 install -r requirements.txt
+pip3 install -r requirements_dev.txt
+pip3 install .
